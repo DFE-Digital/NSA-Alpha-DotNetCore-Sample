91afb78697365c1b55e7fbcbdeeabb7ff871c395
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-08-30 11:06:25 +0100

    Add init script to get runtime for core

--- src/Core2UsersApi/Build.cmd
+++ src/Core2UsersApi/Build.cmd
@@ -1,6 +1,8 @@
 @echo off
 cls
 
+Powershell.exe -executionpolicy remotesigned -File .\Init.ps1
+
 .\.paket\paket.exe update
 
 .\packages\FAKE.Core\tools\FAKE.exe build.fsx publishDirectory=.\..\Publish buildMode=Release
---
+++ src/Core2UsersApi/Init.ps1
@@ -0,0 +1,43 @@
+$ErrorActionPreference="Stop"
+$ProgressPreference="SilentlyContinue"
+
+# $LocalDotnet is the path to the locally-installed SDK to ensure the 
+#   correct version of the tools are executed.
+$LocalDotnet=""
+# $InstallDir and $CliVersion variables can come from options to the 
+#   script.
+$InstallDir = "./cli-tools"
+$CliVersion = "2.0.0"
+
+# Test the path provided by $InstallDir to confirm it exists. If it 
+#   does, it's removed. This is not strictly required, but it's a 
+#   good way to reset the environment.
+if (Test-Path $InstallDir)
+{
+    rm -Recurse $InstallDir
+}
+New-Item -Type "directory" -Path $InstallDir
+
+Write-Host "Downloading the CLI installer..."
+
+# Use the Invoke-WebRequest PowerShell cmdlet to obtain the 
+#   installation script and save it into the installation directory.
+Invoke-WebRequest `
+    -Uri "https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1" `
+    -OutFile "$InstallDir/dotnet-install.ps1"
+
+Write-Host "Installing the CLI requested version ($CliVersion) ..."
+
+# Install the SDK of the version specified in $CliVersion into the 
+#   specified location ($InstallDir).
+& $InstallDir/dotnet-install.ps1 `
+    -InstallDir $InstallDir `
+	-Channel "2.0"
+
+Write-Host "Downloading and installation of the SDK is complete."
+
+# $LocalDotnet holds the path to dotnet.exe for future use by the 
+#   script.
+$LocalDotnet = "$InstallDir/dotnet"
+
+# Run the build process now. Implement your build script here.
